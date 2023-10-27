c6e6a8d168530c0132ec5097600a7e177fd54783
devik01 <devikrishna0301@gmail.com>
2023-10-27 12:08:20 +0530

    redirectlogin

--- redirect.py
+++ redirect.py
@@ -1,10 +1,23 @@
-from flask import Flask,redirect,url_for,request
+from flask import Flask,redirect,url_for,request,render_template
 
 app=Flask(__name__)
 
-@app.route('/redirect2')
+@app.route('/hi')
 def func1():
-    return redirect(url_for('flask.html'))
+    return render_template('flask.html')
+
+
+
+@app.route('/validate', methods = ["POST"] )  
+def validate():  
+    if request.method == 'POST' and request.form['fname'] == 'devi': 
+
+        return redirect(url_for("success"))  
+   
+
+@app.route('/success')
+def success():
+     return "Success!"
 
 if __name__=='__main__':
      app.run(debug=True)
